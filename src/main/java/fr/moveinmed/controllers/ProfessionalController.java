0b7b0b85e1361f1d60a1427e9a8bc2a725e32c63
Melheb younes <melheb.younes@gmail.com>
2021-06-09 19:37:13 +0200

    Fix: type methods in controller

--- src/main/java/fr/moveinmed/controllers/ProfessionalController.java
+++ src/main/java/fr/moveinmed/controllers/ProfessionalController.java
@@ -23,58 +23,58 @@ public class ProfessionalController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity addProfessional(@RequestBody Professional professional) {
+    public ResponseEntity<Professional> addProfessional(@RequestBody Professional professional) {
         professionalService.addProfessional(professional);
         return new ResponseEntity<>(professional, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity getAllProfessional(@RequestParam(required = false) String firstName, String lastName) {
+    public ResponseEntity<List<Professional>> getAllProfessional(@RequestParam(required = false) String firstName, String lastName) {
         if (StringUtils.isEmpty(firstName) && StringUtils.isEmpty(lastName)) {
             List<Professional> professionals = professionalService.getListProfessionals();
             if (professionals != null) {
                 return new ResponseEntity<>(professionals,HttpStatus.OK);
             }
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         List<Professional> professionals = professionalService.findAllByFirstNameContaining(firstName);
         return new ResponseEntity<>(professionals, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getProfessionalById(@PathVariable("id") long id) {
+    public ResponseEntity<Professional> getProfessionalById(@PathVariable("id") long id) {
         Professional professional = professionalService.findProfessionalById(id);
         if (professional != null) {
             return new ResponseEntity<>(professional, HttpStatus.OK);
         }
-        return new ResponseEntity(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("/search")
-    public ResponseEntity getProfessionalByName(@Param("name") String name) {
+    public ResponseEntity<List<Professional>> getProfessionalByName(@Param("name") String name) {
         List<Professional> professionals = professionalService.findAllByFirstNameContaining(name);
         if (professionals != null) {
             return new ResponseEntity<>(professionals, HttpStatus.OK);
         }
-        return new ResponseEntity(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity updateProfessional(@PathVariable("id") Long id, @RequestBody Professional professional) {
+    public ResponseEntity<Professional> updateProfessional(@PathVariable("id") Long id, @RequestBody Professional professional) {
         if (professionalService.findProfessionalById(id) != null) {
             professionalService.updateProfessional(professional, id);
             return new ResponseEntity<>(professional, HttpStatus.OK);
         }
-        return new ResponseEntity(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteProfessional(@PathVariable("id") Long id) {
+    public ResponseEntity<Object> deleteProfessional(@PathVariable("id") Long id) {
         Professional professional = professionalService.findProfessionalById(id);
         if (professional != null) {
             professionalService.deleteProfessional(id);
-            return new ResponseEntity(HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.OK);
         }
-        return new ResponseEntity(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
